10bdcac2d380be0bf94e09283b4dfb9aedbae1bf
GiovanniIacuzzo <iacuzzogiovanni02@gmail.com>
2025-10-17 11:56:56 +0200

    modifica al main simulink e aggiunta dei sensori sensiron SHT31 che misurano temperatura e umidità

---
+++ Sensore_temp_umid_fnctn.m
@@ -0,0 +1,14 @@
+function[exectime, data] = Sensore_temp_umid_fnctn(segment, data)
+
+switch segment
+    case 1
+        data.msg.messaggio = ttAnalogIn(1);
+        exectime = 0.03;
+    case 2
+        data.msg.type = 'temp_umid_signal';
+        data.msg.timeStamp = ttCurrentTime;
+        % ttSendMsg(4, data.msg, 80);
+        exectime = 0.02;
+    case 3
+        exectime = -1;
+end
---
+++ sensore_SensirionSHT31_init.m
@@ -0,0 +1,41 @@
+function sensore_SensirionSHT31_init
+
+ttInitKernel('prioFP');
+
+ttCreateMailbox('power_signal', 10);
+ttSetKernelParameter('energyconsumption', 0.010);
+
+% creazione del task sensore
+nome_s = 'Task_Sensore_temp_umid';
+startTime = 0.0;
+periodo = 1;
+funzione = 'Sensore_temp_umid_fnctn';
+
+% task periodico
+ttCreatePeriodicTask(nome_s, startTime, periodo, funzione);
+
+prio = 1;
+ttSetPriority(prio, nome_s);
+
+nome_s1 = 'Task_Potenza';
+deadline = 0.010;
+funzione1 = 'Potenza_fnctn';
+
+% creazione del Task
+ttCreateTask(nome_s1, deadline, funzione1);
+
+prio = 2;
+ttSetPriority(prio, nome_s1);
+
+% creazione del network handler
+nome_nh = 'nw_handlersens';
+funzione_nh = 'nethandsens_fnctn';
+
+ttCreateHandler(nome_nh, 1, funzione_nh);
+ttAttachNetworkHandler(1, nome_nh);
+
+% Log Sensore Temperatura
+ttCreateLog(nome_s, 1, 'TemperaturaResponseTime', 2000);
+ttCreateLog(nome_s, 2, 'TemperaturaReleaseLatency', 2000);
+ttCreateLog(nome_s, 3, 'TemperaturaStartLatency', 2000);
+ttCreateLog(nome_s, 4, 'TemperaturaExecutionTime', 2000);
